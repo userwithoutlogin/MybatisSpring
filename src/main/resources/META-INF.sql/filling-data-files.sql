278f62329a1543f1add7fa2d546ea9ea6b747513
userwithoutname <bchemodanovich@bk.ru>
2018-07-10 18:28:38 +0400

    fill users and authors tables

--- src/main/resources/META-INF.sql/filling-data-files.sql
+++ src/main/resources/META-INF.sql/filling-data-files.sql
@@ -1,10 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  root
- * Created: 10.07.2018
- */
+insert into users(user_name,password) values
+('John','Doe'),
+('Chris','Scifer'),
+('Sam','Johnson'),
+('Paul Watson');
 
+insert into authors(real_name,id_card,user_id) values
+('Eric','er1234','1'),
+('Stan','3r4434','2'),
+('Rob','3rwe34','3'),
+('Jason','3rfse4','4');
