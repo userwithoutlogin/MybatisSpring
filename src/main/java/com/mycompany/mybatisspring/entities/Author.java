5e166fe6d542e21b21c2e9dfac347b175e84b775
userwithoutname <bchemodanovich@bk.ru>
2018-07-10 18:24:27 +0400

    create User and Author entities

---
+++ src/main/java/com/mycompany/mybatisspring/entities/Author.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mybatisspring.entities;
+
+/**
+ *
+ * @author root
+ */
+public class Author {
+    private Long id;
+    private String realName;
+    private String idCard;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/mybatisspring/entities/User.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mybatisspring.entities;
+
+/**
+ *
+ * @author root
+ */
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
