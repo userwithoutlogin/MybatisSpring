2a9fd8508e062593a66a65b1ffa9da25c7d9b07a
userwithoutname <bchemodanovich@bk.ru>
2018-07-10 17:39:52 +0400

    data base tables initialize

--- src/main/resources/META-INF.sql/schemas.sql
+++ src/main/resources/META-INF.sql/schemas.sql
@@ -1,10 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  root
- * Created: 10.07.2018
- */
+create table users(
+id int unsigned not null auto_increment primary key,
+user_name varchar(10) not null,
+password varchar(5) not null 
+);
 
+create table author(
+id int unsigned not null auto_increment primary key,
+real_name varchar(10) not null,
+id_card varchar(15) not null,
+user_id int unsigned not null,
+unique un_ui (user_id),
+constraint fk_user  
+foreign key (user_id) references users(id)
+)
